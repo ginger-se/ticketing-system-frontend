922f9ec993f64384df6677bbeb8744e22b88eb4d
Daniel T. Fijalka <daniel.fijalka@hobbylobby.com>
2026-06-05 16:13:22 -0500

    feat: complete UI updates for Login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,6 +6,8 @@ import UserServices from "../services/UserServices.js";
 
 const router = useRouter();
 const isCreateAccount = ref(false);
+const visible = ref(false);
+const buttonClass = ref('button-style');
 const snackbar = ref({
   value: false,
   color: "",
@@ -48,14 +50,12 @@ async function createAccount() {
 async function login() {
   await UserServices.loginUser(user)
     .then((data) => {
-            console.log("Login response:", data.data); // ← add this
-
       window.localStorage.setItem("user", JSON.stringify(data.data));
       snackbar.value.value = true;
       snackbar.value.color = "green";
       snackbar.value.text = "Login successful!";
       if (data.data.userType === "admin") {
-        router.push({ name: "adminDashboard" });
+        router.push({ name: "adminShows" });
       } 
       else {router.push({ name: "shows" });}
     })
@@ -81,17 +81,97 @@ function closeSnackBar() {
 </script>
 
 <template>
-  <v-container>
-    <div id="body">
+  <v-container fluid class="fill-height">
+    <v-row justify="center">
+      <v-col cols="6">
+        <v-card class="rounded-lg elevation-5 px-8">
+        
+          <v-card-title class="headline my-4 text-center">
+            Welcome to Planetarium
+          </v-card-title>
+               
+          <v-card-text>
+            <div class="text-body-large text-large-emphasis mb-1">Email</div>
+            <v-text-field
+              v-model="user.email"
+              density="compact"
+              placeholder="jane.doe@gmail.com"
+              prepend-inner-icon="mdi-email-outline"
+              variant="outlined"
+              required
+            ></v-text-field>
+    
+            <div class="text-body-large text-large-emphasis mb-1">Password</div>
+            <v-text-field
+                v-model="user.password"
+                :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                :type="visible ? 'text' : 'password'"
+                density="compact"
+                placeholder="abc123"
+                prepend-inner-icon="mdi-lock-outline"
+                variant="outlined"
+                @click:append-inner="visible = !visible"
+                required
+            ></v-text-field>
+            
+            <div id="buttonWrapper" class="d-flex align-center flex-column mt-4 mb-2">
+              <v-btn :class="buttonClass" color="primary" @click="login()">Log In</v-btn>
+              <v-container class="my-2">
+                <v-divider>or</v-divider>              
+              </v-container>
+              <v-btn class="mb-2" :class="buttonClass" color="primary" variant="tonal" @click="openCreateAccount()">Create New Account</v-btn>
+              <v-btn class="my-2" :class="buttonClass" color="seconday" variant="outlined">Continue as Guest</v-btn>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+      
+    <v-dialog persistent v-model="isCreateAccount" width="800">
       <v-card class="rounded-lg elevation-5">
-        <v-card-title class="headline mb-2">Login </v-card-title>
+        <v-toolbar color="primary">
+          <v-toolbar-title>
+            Create Your Account
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-toolbar>
+    
         <v-card-text>
+    
+          <v-alert>
+            Create an account to save your tickets, manage bookings,
+            and get exclusive updates!
+          </v-alert>
+    
+          <v-row>
+            <v-col>
+              <v-text-field
+                v-model="user.firstName"
+                label="First Name"
+                required
+              >
+              </v-text-field>
+            </v-col>
+            <v-col>
+              <v-text-field
+                v-model="user.lastName"
+                label="Last Name"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+    
+          <v-divider></v-divider>
+    
           <v-text-field
             v-model="user.email"
             label="Email"
             required
           ></v-text-field>
-
+    
           <v-text-field
             v-model="user.password"
             label="Password"
@@ -99,73 +179,38 @@ function closeSnackBar() {
           ></v-text-field>
         </v-card-text>
         <v-card-actions>
-          <v-btn variant="flat" color="secondary" @click="openCreateAccount()"
+          <v-spacer></v-spacer>
+          <v-btn
+            variant="flat"
+            color="secondary"
+            @click="closeCreateAccount()"
+            >Close</v-btn
+          >
+          <v-btn variant="flat" color="primary" @click="createAccount()"
             >Create Account</v-btn
           >
-          <v-spacer></v-spacer>
-
-          <v-btn variant="flat" color="primary" @click="login()">Login</v-btn>
         </v-card-actions>
       </v-card>
-
-     
-
-      <v-dialog persistent v-model="isCreateAccount" width="800">
-        <v-card class="rounded-lg elevation-5">
-          <v-card-title class="headline mb-2">Create Account </v-card-title>
-          <v-card-text>
-            <v-text-field
-              v-model="user.firstName"
-              label="First Name"
-              required
-            ></v-text-field>
-
-            <v-text-field
-              v-model="user.lastName"
-              label="Last Name"
-              required
-            ></v-text-field>
-
-            <v-text-field
-              v-model="user.email"
-              label="Email"
-              required
-            ></v-text-field>
-
-            <v-text-field
-              v-model="user.password"
-              label="Password"
-              required
-            ></v-text-field>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              variant="flat"
-              color="secondary"
-              @click="closeCreateAccount()"
-              >Close</v-btn
-            >
-            <v-btn variant="flat" color="primary" @click="createAccount()"
-              >Create Account</v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-
-      <v-snackbar v-model="snackbar.value" rounded="pill">
-        {{ snackbar.text }}
-
-        <template v-slot:actions>
-          <v-btn
-            :color="snackbar.color"
-            variant="text"
-            @click="closeSnackBar()"
-          >
-            Close
-          </v-btn>
-        </template>
-      </v-snackbar>
-    </div>
+    </v-dialog>
+    
+    <v-snackbar v-model="snackbar.value" rounded="pill">
+      {{ snackbar.text }}
+    
+      <template v-slot:actions>
+        <v-btn
+          :color="snackbar.color"
+          variant="text"
+          @click="closeSnackBar()"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-container>
 </template>
+
+<style scoped>
+.button-style {
+  width: 48%;
+}
+</style>
