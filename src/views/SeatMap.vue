22f9053a32a2b823d0c539ecf97be06a4d0a6900
Daniel Fijalka <aklajifd@gmail.com>
2026-06-13 13:22:06 -0500

    feat: added initial view of the seat map and its routes

---
+++ src/views/SeatMap.vue
@@ -0,0 +1,248 @@
+<script setup>
+import { onMounted } from "vue";
+import { ref, toRaw, computed, shallowRef } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const screen = ref('screen');
+const seatingCard = ref('seating-card');
+const seatingContainer = ref('seating-container');
+const selectedSeats = ref([]);
+const selected = ref('selected');
+const taken = ref('taken');
+const wheelchair = ref('wheelchair');
+const rowHeaders = ref('row-headers');
+const colHeaders = ref('col-headers');
+const seatColor = ref('seat-color');
+const legendColor = ref('legend-color');
+const continueButton = ref('continue-button');
+const isNonClickableButton = shallowRef(true);
+
+const numberOfSelectedSeats = computed(() => {
+  return selectedSeats.value.length;
+});
+
+const seatMap = ref([
+  [ 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1 ],
+  [ 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1 ],
+  [ 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1 ],
+  [ 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1 ],
+  [ 0, 0, 0, 1, 2, 1, 2, 2, 1, 2, 1, 0, 0, 0 ],
+  [ 2, 2, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2 ],
+  [ 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1 ],
+  [ 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1 ],
+  [ 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1 ],
+  [ 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1 ],
+]);
+
+const headerRow = ['', 1, 2, '', 3, 4, 5, 6, 7, 8, 9, 10, '', 11, 12];
+
+const snackbar = ref({
+  value: false,
+  color: "",
+  text: "",
+});
+
+onMounted(async () => {
+});
+
+function closeSnackBar() {
+  snackbar.value.value = false;
+}
+
+function mapNumberToLetter(num) {
+  return String.fromCharCode(num + 65);
+}
+
+function locateSeat(rowIndex, colIndex) {
+  const rowLetter = mapNumberToLetter(rowIndex);
+  const seatNum = colIndex + 1;
+  const index = selectedSeats.value.findIndex(seat => seat.row === rowLetter && seat.seatNumber === seatNum);
+
+  return { rowLetter, seatNum, index };
+}
+
+function seatToggle(rowIndex, colIndex) {
+  const { rowLetter, seatNum, index } = locateSeat(rowIndex, colIndex);
+
+  if (index === -1) {
+    selectedSeats.value.push({
+      row: rowLetter,
+      seatNumber: seatNum
+    });
+  }
+  else {
+    selectedSeats.value.splice(index, 1);
+  }
+  
+  console.log(selectedSeats.value);
+}
+
+function isSeatSelected(rowIndex, colIndex) {
+  return locateSeat(rowIndex, colIndex).index !== -1;
+}
+</script>
+
+<template>
+  <v-container>
+    <div id="body">
+      <v-row id="reservationTimer">
+        <h3>Select Your Seats</h3>
+      </v-row>
+
+      <v-row justify="center" class="mt-7">
+        <v-card :class="seatingCard" class="rounded-md elevation-2">
+          <v-row justify="center" :class="screen" class="mb-3 bg-grey-darken-4">SCREEN</v-row>
+          <div :class="seatingContainer">
+            <!-- Generate the column headers -->
+            <template v-for="header in headerRow" :key="header">
+              <div>
+                <v-btn variant="text" :class="colHeaders" :readonly="isNonClickableButton">
+                  {{ header }}
+                </v-btn>
+              </div>
+            </template>
+
+            <!-- Loop through the rows in the seat map -->
+            <template v-for="(row, rowIndex) in seatMap" :key="rowIndex">
+              <div>
+                <v-btn variant="text" :class="rowHeaders" :readonly="isNonClickableButton">
+                  {{ mapNumberToLetter(rowIndex) }}              
+                </v-btn>
+              </div>
+
+              <!-- Loop through the columns in the seat map -->
+              <div v-for="(column, colIndex) in row" :key="colIndex">
+                <!-- Values of 1 in seat map are regular seats -->
+                <div v-if="column === 1" @click="seatToggle(rowIndex, colIndex)">
+                  <v-btn :class="isSeatSelected(rowIndex, colIndex) ? 'selected' : 'seatColor'" variant="text">
+                    <v-icon>
+                      {{ isSeatSelected(rowIndex, colIndex) ? 'mdi-sofa-single' : 'mdi-sofa-single-outline' }}
+                    </v-icon>
+                  </v-btn>
+                </div>
+
+                <!-- Values of 2 in seat map are handicap seats -->
+                <div v-else-if="column === 2" @click="seatToggle(rowIndex, colIndex)">
+                  <v-btn :class="isSeatSelected(rowIndex, colIndex) ? 'selected' : 'wheelchair'" variant="text">
+                    <v-icon>
+                      mdi-wheelchair-accessibility
+                    </v-icon>
+                  </v-btn>
+                </div>
+
+                <!-- Values of 0 in seat map are empty spaces for aisles -->
+                <div v-else-if="column === 0">
+                  <v-btn variant="text" :readonly="isNonClickableButton">
+                  </v-btn>
+                </div>
+              </div>
+            </template>
+          </div>
+
+          <v-row class="mb-4 justify-center ga-6">
+            <v-col class="d-flex ga-2 justify-center" cols="auto">
+              <v-icon :class="seatColor">mdi-sofa-single-outline</v-icon>
+              <v-text :class="legendColor">Available</v-text>
+            </v-col>
+
+            <v-col class="d-flex ga-2 justify-center" cols="auto">
+              <v-icon color="rgb(240, 162, 60)">mdi-sofa-single</v-icon>
+              <v-text :class="legendColor">Selected ({{ numberOfSelectedSeats }})</v-text>
+            </v-col>
+
+            <v-col class="d-flex ga-2 justify-center" cols="auto">
+              <v-icon :class="taken">mdi-sofa-single</v-icon>
+              <v-text :class="legendColor">Taken</v-text>
+            </v-col>
+
+            <v-col class="d-flex ga-2 justify-center" cols="auto">
+              <v-icon :class=wheelchair>mdi-wheelchair-accessibility</v-icon>
+              <v-text :class="legendColor">Wheelchair Accessible</v-text>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-row>
+
+      <v-row class="justify-center pt-4 mb-8 text-center">
+        <v-btn :class="continueButton" color="primary">
+          Continue to Checkout ({{ numberOfSelectedSeats }} {{ numberOfSelectedSeats === 1 ? "seat" : "seats" }})
+        </v-btn>
+      </v-row>
+    </div>
+
+    <v-snackbar v-model="snackbar.value" rounded="pill">
+      {{ snackbar.text }}
+      <template v-slot:actions>
+        <v-btn :color="snackbar.color" variant="text" @click="closeSnackBar()">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </v-container>
+</template>
+
+
+<style scoped>
+.seating-container {
+  padding: 1.4rem 3rem;
+  color: #b3b1b1;
+  display: grid;
+  grid-template-columns: repeat(15, 1fr);
+  grid-template-rows: repeat(11, 1fr);
+}
+
+.screen {
+  padding: 0.4rem 0;
+  border-top-left-radius: 80%;
+  border-top-right-radius: 80%;
+  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  letter-spacing: 0.1rem;
+}
+
+.selected {
+  color: rgb(240, 162, 60);
+  background-color: rgba(250, 196, 120, 0.281);
+  border: 1px solid rgb(206, 211, 212);
+}
+
+.seating-card {
+  padding: 2.2rem 2rem 0;
+  background-color: rgb(90, 88, 84)
+}
+
+#reservationTimer {
+  padding: 1rem;
+}
+
+.wheelchair {
+  color:rgb(142, 248, 248);
+}
+
+.seat-color {
+  color: #b3b1b1;
+}
+
+.legend-color {
+  color: #e0dbdb;
+}
+
+.taken {
+  color: #05010c;
+}
+
+.row-headers {
+  color: #7eff84b7;
+  justify-content: center;
+}
+
+.col-headers {
+  color: #7eff84b7;
+  align-items: start;
+}
+
+.continue-button {
+  width: 94%;
+  height: 3rem;
+}
+</style>
